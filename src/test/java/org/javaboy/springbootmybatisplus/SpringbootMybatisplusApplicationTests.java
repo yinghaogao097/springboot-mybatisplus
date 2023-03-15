282eb8d9193b69745a3a2f709127ea12acf7cdb4
高莹豪 <yinghaogao097@gmail.com>
2023-03-15 11:26:43 +0800

    测试分页查询

--- src/test/java/org/javaboy/springbootmybatisplus/SpringbootMybatisplusApplicationTests.java
+++ src/test/java/org/javaboy/springbootmybatisplus/SpringbootMybatisplusApplicationTests.java
@@ -1,5 +1,6 @@
 package org.javaboy.springbootmybatisplus;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.javaboy.springbootmybatisplus.mapper.UserMapper;
 import org.javaboy.springbootmybatisplus.pojo.User;
 import org.junit.jupiter.api.Test;
@@ -13,6 +14,25 @@ import java.util.Map;
 class SpringbootMybatisplusApplicationTests {
     @Resource
     private UserMapper userMapper;
+    @Test
+    public void testSelectPage(){
+        // 当前页码
+        int page = 2;
+        // 每页数量
+        int size = 3;
+
+        // 构建分页请求条件
+        Page<User> pageReq = new Page<>(page, size);
+
+        // 执行分页请求 结果会回填到pageReq queryWrapper为null则查询所有数据
+        userMapper.selectPage(pageReq, null);
+
+        System.out.println("总页数："+pageReq.getPages());
+        System.out.println("总记录数"+pageReq.getTotal());
+
+        // 当前页数据
+        pageReq.getRecords().forEach(System.out::println);
+    }
 
     @Test
     public void getById() {
@@ -46,7 +66,18 @@ class SpringbootMybatisplusApplicationTests {
         map.put("age", "15");
 
         userMapper.deleteByMap(map);
+
+
     }
 
+    @Test
+    public void testUpdateById() {
+        User user = User.builder().
+                id(2L).
+                password("111111").
+                build();
+        int count = userMapper.updateById(user);
+        System.out.println(count);
+    }
 
 }
