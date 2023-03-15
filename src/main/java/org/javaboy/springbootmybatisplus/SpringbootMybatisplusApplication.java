e3a22064c9add23e4cdee8b1cb451d2a9a35ed9a
高莹豪 <yinghaogao097@gmail.com>
2023-03-15 09:54:33 +0800

    Initial commit

---
+++ src/main/java/org/javaboy/springbootmybatisplus/SpringbootMybatisplusApplication.java
@@ -0,0 +1,13 @@
+package org.javaboy.springbootmybatisplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisplusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMybatisplusApplication.class, args);
+    }
+
+}
