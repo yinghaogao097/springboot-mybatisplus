964a543e20f97ed92d05a67343d780221002c99b
高莹豪 <yinghaogao097@gmail.com>
2023-03-15 10:46:02 +0800

    测试单表数据插入以及指定表名、字段和忽略字段

--- src/main/java/org/javaboy/springbootmybatisplus/pojo/User.java
+++ src/main/java/org/javaboy/springbootmybatisplus/pojo/User.java
@@ -1,5 +1,8 @@
 package org.javaboy.springbootmybatisplus.pojo;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,10 +19,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class User {
+    @TableId(type = IdType.AUTO)
     private Long id;
     private String userName;
     private String password;
     private String name;
     private Integer age;
     private String email;
+    @TableField(exist = false)
+    private String email2;
 }
--- src/test/java/org/javaboy/springbootmybatisplus/SpringbootMybatisplusApplicationTests.java
+++ src/test/java/org/javaboy/springbootmybatisplus/SpringbootMybatisplusApplicationTests.java
@@ -1,6 +1,7 @@
 package org.javaboy.springbootmybatisplus;
 
 import org.javaboy.springbootmybatisplus.mapper.UserMapper;
+import org.javaboy.springbootmybatisplus.pojo.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -14,6 +15,20 @@ class SpringbootMybatisplusApplicationTests {
     @Test
     public void getById() {
         System.out.println(userMapper.selectById(1));
+    }
+
+    @Test
+    public void testInsertData() {
+        User user =
+                User.builder()
+                        .userName("itheima22")
+                        .name("itcast22")
+                        .age(15)
+                        .email("itcast22@itcast.cn")
+                        .password("222222")
+                        .build();
+        int insert = userMapper.insert(user);
+        System.out.println(insert);
     }
 
 
